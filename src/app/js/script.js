2bae9b779cb90f241bed3a52411235397328cc6a
Amanda <amandakehldefarias@hotmail.com>
2022-05-08 21:03:55 -0300

    fix in carousel function

--- src/app/js/script.js
+++ src/app/js/script.js
@@ -1,9 +1,9 @@
 const courseNav = document.querySelector('.all-courses')
 
 document.querySelector('#arrow-right').addEventListener('click', () => {
-    courseNav.scrollLeft += 300;
+    courseNav.scrollLeft += 150;
 });
 
 document.querySelector('#arrow-left').addEventListener('click', () => {
-    courseNav.scrollLeft -= 300;
+    courseNav.scrollLeft -= 150;
 });
